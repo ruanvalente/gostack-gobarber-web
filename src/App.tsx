eefdba1e37e6b00c250e14b5c99d19f3f2640dc2
Ruan Valente <ruanopensuselinux@gmail.com>
2020-08-21 00:47:59 -0300

    add user login with usecontext api :memo:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+
 import SignIn from './pages/SignIn'
 // import SignUp from './pages/SignUp'
+
 import GlobalSyle from './styles/global'
+
+import { AuthProvider } from './context/AuthContext'
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalSyle />
   </>
 )
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,10 @@ import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
 
+import { useAuth } from '../../context/AuthContext'
+
+import getValidationErros from '../../utils/getValidationErros'
+
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { Container, Content, Background } from './styles'
 import logoImage from '../../assets/logo.svg'
@@ -10,28 +14,39 @@ import logoImage from '../../assets/logo.svg'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import getValidationErros from '../../utils/getValidationErros'
+interface SignInFormData {
+  email: string
+  password: string
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
+  const { signIn } = useAuth()
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({})
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail é obrigatório')
-          .email('Entre com um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      })
-      await schema.validate(data, {
-        abortEarly: false,
-      })
-    } catch (error) {
-      const erros = getValidationErros(error)
-      formRef.current?.setErrors(erros)
-    }
-  }, [])
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Entre com um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        })
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+        signIn({
+          email: data.email,
+          password: data.password,
+        })
+      } catch (error) {
+        const erros = getValidationErros(error)
+        formRef.current?.setErrors(erros)
+      }
+    },
+    [signIn],
+  )
 
   return (
     <Container>
@@ -50,10 +65,10 @@ const SignIn: React.FC = () => {
 
           <Button type="submit">Entrar</Button>
 
-          <a href="#">Esqueci minha senha</a>
+          <a href="/forgot">Esqueci minha senha</a>
         </Form>
 
-        <a href="#">
+        <a href="/create">
           <FiLogIn />
           Criar conta
         </a>
